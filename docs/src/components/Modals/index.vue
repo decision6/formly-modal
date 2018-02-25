5d927e7de84d3a76124588fe43aca3b428a3148b
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-02-25 16:43:11 -0300

    added docs folder

---
+++ docs/src/components/Modals/index.vue
@@ -0,0 +1,31 @@
+<script>
+  import { QBtn } from 'quasar-framework'
+  import FormlyModal from 'formly-modal'
+
+  export default {
+    name: 'modals',
+    components: { FormlyModal, QBtn },
+    data: () => ({
+      model: {},
+      fields: []
+    })
+  }
+</script>
+
+<template>
+  <div>
+    <FormlyModal
+      ref="modal"
+      title="Example form"
+      size="lg"
+      :height="400"
+      :model="model"
+      :fields="fields" />
+
+    <QBtn @click="$refs.modal.open()"> Abrir modal </QBtn>
+  </div>
+</template>
+
+<style>
+
+</style>
